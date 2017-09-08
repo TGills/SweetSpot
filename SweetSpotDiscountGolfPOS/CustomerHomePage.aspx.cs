a13735276f15ec1cd61b67c3e09c7da93dcefada
Nathan <nathan_s_scherr@yahoo.ca>
2017-09-08 11:48:30 -0600

    Commented Customer Home Page
    Added comments for to the customer home page for the code behind.

--- SweetSpotDiscountGolfPOS/CustomerHomePage.aspx.cs
+++ SweetSpotDiscountGolfPOS/CustomerHomePage.aspx.cs
@@ -15,99 +15,131 @@ namespace SweetSpotDiscountGolfPOS
         ErrorReporting er = new ErrorReporting();
         protected void Page_Load(object sender, EventArgs e)
         {
+            //Collects current method and page for error tracking
             string method = "Page_Load";
             Session["currPage"] = "CustomerHomePage";
             try
             {
+                //checks if the user has logged in
                 if (Convert.ToBoolean(Session["loggedIn"]) == false)
                 {
+                    //Go back to Login to log in
                     Server.Transfer("LoginPage.aspx", false);
                 }
             }
+            //Exception catch
             catch (Exception ex)
             {
+                //Log employee number
                 int employeeID = Convert.ToInt32(Session["loginEmployeeID"]);
+                //Log current page
                 string currPage = Convert.ToString(Session["currPage"]);
+                //Log all info into error table
                 er.logError(ex, employeeID, currPage, method, this);
-                string prevPage = Convert.ToString(Session["prevPage"]);
+                //string prevPage = Convert.ToString(Session["prevPage"]);
+                //Display message box
                 MessageBox.ShowMessage("An Error has occured and been logged. "
                     + "If you continue to receive this message please contact "
                     + "your system administrator", this);
                 //Server.Transfer(prevPage, false);
             }
         }
-
         protected void btnCustomerSearch_Click(object sender, EventArgs e)
         {
+            //Collects current method and page for error tracking
             string method = "btnCustomerSearch_Click";
             try
             {
+                //Looks through database and returns a list of customers
+                //based on the search criteria entered
                 SweetShopManager ssm = new SweetShopManager();
                 List<Customer> c = ssm.GetCustomerfromSearch(txtSearch.Text);
-
+                //Binds the results to the gridview
                 grdCustomersSearched.Visible = true;
                 grdCustomersSearched.DataSource = c;
                 grdCustomersSearched.DataBind();
             }
+            //Exception catch
             catch (Exception ex)
             {
+                //Log employee number
                 int employeeID = Convert.ToInt32(Session["loginEmployeeID"]);
+                //Log current page
                 string currPage = Convert.ToString(Session["currPage"]);
+                //Log all info into error table
                 er.logError(ex, employeeID, currPage, method, this);
-                string prevPage = Convert.ToString(Session["prevPage"]);
+                //string prevPage = Convert.ToString(Session["prevPage"]);
+                //Display message box
                 MessageBox.ShowMessage("An Error has occured and been logged. "
                     + "If you continue to receive this message please contact "
                     + "your system administrator", this);
                 //Server.Transfer(prevPage, false);
             }
         }
-
         protected void btnAddNewCustomer_Click(object sender, EventArgs e)
         {
+            //Collects current method and page for error tracking
             string method = "btnAddNewCustomer_Click";
             try
             {
+                //Opens the page to add a new customer
                 Server.Transfer("CustomerAddNew.aspx", false);
             }
+            //Exception catch
             catch (Exception ex)
             {
+                //Log employee number
                 int employeeID = Convert.ToInt32(Session["loginEmployeeID"]);
+                //Log current page
                 string currPage = Convert.ToString(Session["currPage"]);
+                //Log all info into error table
                 er.logError(ex, employeeID, currPage, method, this);
-                string prevPage = Convert.ToString(Session["prevPage"]);
+                //string prevPage = Convert.ToString(Session["prevPage"]);
+                //Display message box
                 MessageBox.ShowMessage("An Error has occured and been logged. "
                     + "If you continue to receive this message please contact "
                     + "your system administrator", this);
                 //Server.Transfer(prevPage, false);
             }
         }
-
         protected void grdCustomersSearched_RowCommand(object sender, GridViewCommandEventArgs e)
         {
+            //Collects current method and page for error tracking
             string method = "grdCustomersSearched_RowCommand";
             try
             {
+                //grabs the command argument for the command pressed 
                 string key = e.CommandArgument.ToString();
                 if (e.CommandName == "ViewProfile")
                 {
+                    //if command argument is view profile 
                     Session["key"] = key;
+                    //then set key and open Add New Customer page
                     Server.Transfer("CustomerAddNew.aspx", false);
-
                 }
                 else if (e.CommandName == "StartSale")
                 {
+                    //command argument is start sale
                     Session["returnedFromCart"] = false;
+                    //Set transaction type to sale
                     Session["TranType"] = 1;
+                    //Set key to the customer number
                     Session["key"] = key;
+                    //Open the Sales Cart page
                     Server.Transfer("SalesCart.aspx", false);
                 }
             }
+            //Exception catch
             catch (Exception ex)
             {
+                //Log employee number
                 int employeeID = Convert.ToInt32(Session["loginEmployeeID"]);
+                //Log current page
                 string currPage = Convert.ToString(Session["currPage"]);
+                //Log all info into error table
                 er.logError(ex, employeeID, currPage, method, this);
-                string prevPage = Convert.ToString(Session["prevPage"]);
+                //string prevPage = Convert.ToString(Session["prevPage"]);
+                //Display message box
                 MessageBox.ShowMessage("An Error has occured and been logged. "
                     + "If you continue to receive this message please contact "
                     + "your system administrator", this);
