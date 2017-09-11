081e2a1b3890926f0aa1a9099de0136fe88cabaf
Nathan <nathan_s_scherr@yahoo.ca>
2017-09-11 11:56:45 -0600

    Inventory Home Page Comments
    Added comments to the code behind of the inventory home page.

--- SweetSpotDiscountGolfPOS/InventoryHomePage.aspx.cs
+++ SweetSpotDiscountGolfPOS/InventoryHomePage.aspx.cs
@@ -15,34 +15,44 @@ namespace SweetSpotDiscountGolfPOS
         ErrorReporting er = new ErrorReporting();
         protected void Page_Load(object sender, EventArgs e)
         {
+            //Collects current method and page for error tracking
             string method = "Page_Load";
             Session["currPage"] = "InventoryHomePage";
             try
             {
+                //checks if the user has logged in
                 if (Convert.ToBoolean(Session["loggedIn"]) == false)
                 {
+                    //Go back to Login to log in
                     Server.Transfer("LoginPage.aspx", false);
                 }
+
                 if (Session["Admin"] == null)
                 {
+                    //If user is not an admin then disable the add new item button
                     btnAddNewInventory.Enabled = false;
                 }
             }
+            //Exception catch
             catch (Exception ex)
             {
+                //Log employee number
                 int employeeID = Convert.ToInt32(Session["loginEmployeeID"]);
+                //Log current page
                 string currPage = Convert.ToString(Session["currPage"]);
+                //Log all info into error table
                 er.logError(ex, employeeID, currPage, method, this);
-                string prevPage = Convert.ToString(Session["prevPage"]);
+                //string prevPage = Convert.ToString(Session["prevPage"]);
+                //Display message box
                 MessageBox.ShowMessage("An Error has occured and been logged. "
                     + "If you continue to receive this message please contact "
                     + "your system administrator", this);
                 //Server.Transfer(prevPage, false);
             }
         }
-
         protected void btnInventorySearch_Click(object sender, EventArgs e)
         {
+            //Collects current method for error tracking
             string method = "btnInventorySearch_Click";
             try
             {
@@ -56,16 +66,20 @@ namespace SweetSpotDiscountGolfPOS
                 }
                 else
                 {
+                    //If teaxt has been entered to search use it to dislpay relevent items
                     string loc = Convert.ToString(Session["Loc"]);
                     SweetShopManager ssm = new SweetShopManager();
                     string itemType = ddlInventoryType.SelectedItem.ToString();
+                    //determines if the searched text is a sku number
                     if (!int.TryParse(txtSearch.Text, out skuInt))
                     {
+                        //If number search through skus for any that match
                         skuString = txtSearch.Text;
                         searched = ssm.GetItemfromSearch(txtSearch.Text, itemType);
                     }
                     else
                     {
+                        //If search is text 
                         skuString = txtSearch.Text;
                         // this looks for the item in the database
                         List<Items> i = idu.getItemByID(Convert.ToInt32(skuInt));
@@ -76,67 +90,88 @@ namespace SweetSpotDiscountGolfPOS
                             searched.Add(i.ElementAt(0));
                         }
                     }
+                    //Sets item type
                     Session["itemType"] = itemType;
                     grdInventorySearched.Visible = true;
+                    //Binds returned items to gridview for display
                     grdInventorySearched.DataSource = searched;
                     grdInventorySearched.DataBind();
                 }
             }
+            //Exception catch
             catch (Exception ex)
             {
+                //Log employee number
                 int employeeID = Convert.ToInt32(Session["loginEmployeeID"]);
+                //Log current page
                 string currPage = Convert.ToString(Session["currPage"]);
+                //Log all info into error table
                 er.logError(ex, employeeID, currPage, method, this);
-                string prevPage = Convert.ToString(Session["prevPage"]);
+                //string prevPage = Convert.ToString(Session["prevPage"]);
+                //Display message box
                 MessageBox.ShowMessage("An Error has occured and been logged. "
                     + "If you continue to receive this message please contact "
                     + "your system administrator", this);
                 //Server.Transfer(prevPage, false);
             }
         }
-
         protected void btnAddNewInventory_Click(object sender, EventArgs e)
         {
+            //Collects current method for error tracking
             string method = "btnAddNewInventory_Click";
             try
             {
+                //Changes page to the inventory add new page
                 Server.Transfer("InventoryAddNew.aspx", false);
             }
+            //Exception catch
             catch (Exception ex)
             {
+                //Log employee number
                 int employeeID = Convert.ToInt32(Session["loginEmployeeID"]);
+                //Log current page
                 string currPage = Convert.ToString(Session["currPage"]);
+                //Log all info into error table
                 er.logError(ex, employeeID, currPage, method, this);
-                string prevPage = Convert.ToString(Session["prevPage"]);
+                //string prevPage = Convert.ToString(Session["prevPage"]);
+                //Display message box
                 MessageBox.ShowMessage("An Error has occured and been logged. "
                     + "If you continue to receive this message please contact "
                     + "your system administrator", this);
                 //Server.Transfer(prevPage, false);
             }
         }
-
         protected void grdInventorySearched_RowCommand(object sender, GridViewCommandEventArgs e)
         {
+            //Collects current method for error tracking
             string method = "err_grdInventorySearched_RowCommand";
             try
             {
+                //Stores sku number of selected item
                 string itemKey = e.CommandArgument.ToString();
                 if (e.CommandName == "viewItem")
                 {
+                    //If the command selected is viewItem, store item type 
                     Session["itemKey"] = itemKey;
+                    //Change to Inventory Add new page to display selected item
                     Server.Transfer("InventoryAddNew.aspx");
                 }
             }
+            //Exception catch
             catch (Exception ex)
             {
+                //Log employee number
                 int employeeID = Convert.ToInt32(Session["loginEmployeeID"]);
+                //Log current page
                 string currPage = Convert.ToString(Session["currPage"]);
+                //Log all info into error table
                 er.logError(ex, employeeID, currPage, method, this);
-                string prevPage = Convert.ToString(Session["prevPage"]);
+                //string prevPage = Convert.ToString(Session["prevPage"]);
+                //Display message box
                 MessageBox.ShowMessage("An Error has occured and been logged. "
                     + "If you continue to receive this message please contact "
                     + "your system administrator", this);
-                //Server.Transfer(prevPage);
+                //Server.Transfer(prevPage, false);
             }
         }
     }
